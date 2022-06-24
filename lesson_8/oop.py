6873ba1c3d8135e6ba3522988cabd01c862387fd
DmytroMoiseienko7 <101639069+DmytroMoiseienko7@users.noreply.github.com>
2022-06-24 19:46:24 +0300

    Task is completed

---
+++ lesson_8/oop.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractclassmethod
+from random import choice
+
+
+class Shape(ABC):
+    @abstractclassmethod
+    def draw(self):
+        pass
+
+
+class Rectangle(Shape):
+    def draw():
+        print(" ---- \n |  | \n ----")
+
+
+class Circle(Shape):
+    def draw():
+        print("  -- \n -  - \n  --")
+
+
+def get_shape() -> Shape:
+    """
+    This function should return any instance of a Shape class
+    In our example it is Rectangle or Circle
+    """
+    circle = Circle
+    rectangle = Rectangle
+    options = [circle, rectangle]
+    return choice(options)
+
+
+def main():
+    """
+    If Rectangle is used I'd like to see:
+
+    ----
+    |  |
+    ----
+
+    If Circle is used:
+      --
+     -  -
+      --
+    """
+
+    shape: Shape = get_shape()
+    shape.draw()
+
+
+if __name__ == "__main__":
+    main()
