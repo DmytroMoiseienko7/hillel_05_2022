219685a6afeb6ac34ab78226b3bf621ad9083303
DmytroMoiseienko7 <101639069+DmytroMoiseienko7@users.noreply.github.com>
2022-07-11 23:09:44 +0300

    Move while cycle to def main

--- lesson_11/Home_work/threads.py
+++ lesson_11/Home_work/threads.py
@@ -1,5 +1,6 @@
 import random
 import threading
+from time import sleep
 
 my_list = []
 max_num = 1_000_000
@@ -13,24 +14,14 @@ def list_creation(max_num, my_list):
     print("Сalculations done")
 
 
-def get_sum(my_list, thread):
+def get_sum(my_list):
     print("Starting Thread_2")
-    while True:
-        if thread.is_alive() is True:
-            True
-        else:
-            print("Sum ======>", sum(my_list))
-            break
+    print("Sum ======>", sum(my_list))
 
 
-def get_average(max_num, my_list, thread):
+def get_average(max_num, my_list):
     print("Starting Thread_3")
-    while True:
-        if thread.is_alive() is True:
-            True
-        else:
-            print("Average ======>", sum(my_list) / max_num)
-            break
+    print("Average ======>", sum(my_list) / max_num)
 
 
 def main():
@@ -38,21 +29,20 @@ def main():
     thread_1 = threading.Thread(
         target=list_creation, kwargs={"max_num": max_num, "my_list": my_list}
     )
-    thread_2 = threading.Thread(
-        target=get_sum, kwargs={"my_list": my_list, "thread": thread_1}
-    )
+    thread_2 = threading.Thread(target=get_sum, kwargs={"my_list": my_list})
     thread_3 = threading.Thread(
-        target=get_average,
-        kwargs={"max_num": max_num, "my_list": my_list, "thread": thread_1},
+        target=get_average, kwargs={"max_num": max_num, "my_list": my_list}
     )
 
     thread_1.start()
-    thread_2.start()
-    thread_3.start()
 
-    thread_1.join()
-    thread_2.join()
-    thread_3.join()
+    while True:
+        if thread_1.is_alive():
+            sleep(1)
+        else:
+            thread_2.start()
+            thread_3.start()
+            break
 
 
 if __name__ == "__main__":
